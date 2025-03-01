23c97ddf92e1f7566bd14e2a776a3fa220200524
ajs <140010746+aboodJS@users.noreply.github.com>
2025-03-01 15:10:56 +0300

    changes: made the info section resposive

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ const menuBtn = document.querySelector("#menu-btn");
 const dropMenu = document.querySelector("#drop-menu");
 let show = false;
 
+if (show === false) {
+  dropMenu.style.height = "0px";
+} else {
+  dropMenu.style.height = "25vh";
+}
+
 menuBtn.addEventListener("click", () => {
   show = !show;
   if (show === false) {
